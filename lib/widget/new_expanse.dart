3fe62948f3760bd271375a9aa418aa7789752d51
ramywael <ramybakry9@gmail.com>
2023-10-11 22:46:40 +0300

    Create the DatePicker and displays it

--- lib/widget/new_expanse.dart
+++ lib/widget/new_expanse.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewExpanse extends StatefulWidget {
   const NewExpanse({Key? key}) : super(key: key);
@@ -10,9 +11,13 @@ class NewExpanse extends StatefulWidget {
 }
 
 class _NewExpanseState extends State<NewExpanse> {
+  final firstDate = DateTime(
+      DateTime.now().year - 1, DateTime.now().month, DateTime.now().day);
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _amountController = TextEditingController();
-
+  final now = DateTime.now();
+  DateTime? _selectedDate;
+  final formatter = DateFormat.yMd();
   @override
   void dispose() {
     super.dispose();
@@ -35,20 +40,57 @@ class _NewExpanseState extends State<NewExpanse> {
             keyboardType: TextInputType
                 .text, //the max number of characters that the user can enter
           ),
-          TextField(
-            controller: _amountController,
-            maxLength: 50,
-            decoration: const InputDecoration(
-              label: Text("Amount"),
-            ),
-            keyboardType: TextInputType
-                .number, //the max number of characters that the user can enter
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  maxLength: 50,
+                  decoration: const InputDecoration(
+                    label: Text("Amount"),
+                    prefixText: "\$",
+                  ),
+                  keyboardType: TextInputType
+                      .number, //the max number of characters that the user can enter
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(_selectedDate == null
+                        ? "No Date Selected"
+                        : formatter.format(_selectedDate!)),
+                    IconButton(
+                      onPressed: () async {
+
+                        final DateTime? pickedData = await showDatePicker(
+                          context: context,
+                          initialDate: now,
+                          firstDate: firstDate,
+                          lastDate: now,
+                        );
+                        setState(() {
+                          _selectedDate = pickedData;
+                        });
+                      },
+                      icon: const Icon(Icons.calendar_today),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
           Row(
             children: [
               ElevatedButton(
                 onPressed: () {
                   log(_titleController.text);
+                  _titleController.clear();
+                  _amountController.clear();
                 },
                 child: const Text("Save Expanse"),
               ),
