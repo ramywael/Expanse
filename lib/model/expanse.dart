2b3792ba8c73ef626f6adf1edbc00fa05073cf9e
ramywael <ramybakry9@gmail.com>
2023-10-11 18:22:54 +0300

    Create the enum category

--- lib/model/expanse.dart
+++ lib/model/expanse.dart
@@ -1,13 +1,17 @@
  import "package:uuid/uuid.dart";
 
  const uuid= Uuid(); // it will generate a random id for the id property for each expanse
+ enum Category  {food,work,travel,entertainment,other,}
+ // enum looks like the class but with specific properties
 class ExpanseModel {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 
   ExpanseModel({
+    required this.category,
     required this.title,
     required this.amount,
     required this.date,
